f991c4a38b05bb96e9896ed506018bfafc5dcf32
Vitalii Nikolenko <buhtu4ek@gmail.com>
2016-08-16 14:57:53 +0300

    Добавил цветовые константы

--- nikolenko/uniconsole.pas
+++ nikolenko/uniconsole.pas
@@ -1,6 +1,28 @@
 unit uniconsole;
 
 interface
+
+const
+	// Color constants
+	Black = 0;
+	Blue = 1;
+	Green = 2;
+	Cyan = 3;
+	Red = 4;
+	Magenta = 5;
+	Brown = 6;
+	LightGray = 7;
+	DarkGray = 8;
+	LightBlue = 9;
+	LightGreen = 10;
+	LightCyan = 11;
+	LightRed = 12;
+	LightMagenta = 13;
+	Yellow = 14;
+	White = 15;
+	Blink = 128;
+
+	// Procedures
 	procedure SetTitle(title: pchar);
 	
 	procedure TextColor(col: integer; bkCol: integer);
