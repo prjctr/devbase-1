3431c75239f50b3eba66579acb6124401ed11c98
Artem Novikov <artem.247@gmail.com>
2016-08-19 23:24:38 +0300

    Age.pas updated

--- novikov/age.pas
+++ novikov/age.pas
@@ -9,25 +9,27 @@ begin
 	if Age > 0 then
 	begin
 	writeln('Ok!');
-	end
+		if Age < 50 then
+		begin
+			if Age < 16 then
+				begin
+				message := 'Person is young';
+				end
+			else
+				begin
+				message := 'Person is grown-up';
+				end
+		end
+		
+		else 
+			message := 'Person is old';
+		end
+	
 	else 
 	begin
 	writeln('He is not born yet');
 	end;
-	if Age < 50 then
-	begin
-		if Age < 16 then
-		begin
-			message := 'Person is young'
-		end
-		else
-		begin
-			message := 'Person is grown-up'
-		end;
-
-
-	end
-	else message := 'Person is old';
+	
 
 	writeln(message);
 	readln();
