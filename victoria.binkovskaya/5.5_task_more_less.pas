331eac493999517dc559039ef942dc9878284de3
bekindtoday <binkovskaya22@gmail.com>
2016-08-21 14:45:44 +0300

    new tasks

---
+++ victoria.binkovskaya/5.5_task_more_less.pas
@@ -0,0 +1,36 @@
+program task5_5;
+uses crt;
+
+var 
+	i, number: Integer;
+
+begin
+	Randomize;
+	i := random(100) + 1;
+
+	repeat 
+
+	writeln('Enter the number, please');
+	readln(number);
+
+	if (number <= 0) or (number > 100) then
+	begin
+		writeln('Entering incorrect data');
+		continue;
+	end;
+
+		if number < i then
+		begin
+			writeln('more');
+		end;
+			
+		if number > i then
+		begin
+			writeln('less');
+		end;
+		
+	until number = i;
+
+	writeln('You win!');
+	readln();
+end.
